d500295c844ecc00a0f06b31d5ffcd358df3a0b4
Sangmin Lee <20171687@edu.hanbat.ac.kr>
2022-01-20 09:50:40 +0900

    upload tutorial files

---
+++ src/main/java/io/wisoft/jedis/JedisPoolExample2.java
@@ -0,0 +1,30 @@
+package io.wisoft.jedis;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.Map;
+
+public class JedisPoolExample2 {
+  public static void main(String[] args) {
+    final GenericObjectPoolConfig<Jedis> config = new GenericObjectPoolConfig<>();
+    config.setMaxTotal(20);
+    config.setBlockWhenExhausted(true);
+
+    final JedisPool pool = new JedisPool(config, "127.0.0.1", 6379);
+    final Jedis firstClient = pool.getResource();
+    firstClient.hset("info:sangmin", "이름", "상민");
+    firstClient.hset("info:sangmin", "생일", "1998-05-18");
+
+    final Jedis secondClient = pool.getResource();
+    final Map<String, String> result = secondClient.hgetAll("info:sangmin");
+    System.out.println("이름: " + result.get("이름"));
+    System.out.println("생일: " + result.get("생일"));
+
+    firstClient.close();
+    secondClient.close();
+    pool.close();
+  }
+
+}
